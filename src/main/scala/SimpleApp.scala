147552bd0f68aabe4d93e699c51d12d2cdc3c948
Shivendra Kushwah <shivendrakushwah@gmail.com>
2018-09-06 17:20:38 -0700

    added run command

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -1,6 +1,11 @@
 /* SimpleApp.scala */
 import org.apache.spark.sql.SparkSession
 
+//run this command
+/*
+$SPARK_HOME/bin/spark-submit   --class "SimpleApp"   --master local[4]   target/scala-2.11/simple-project_2.11-1.0.jar
+*/
+
 object SimpleApp {
   def main(args: Array[String]) {
     val logFile = "/Users/Shiv/Desktop/test.txt" // Should be some file on your system
